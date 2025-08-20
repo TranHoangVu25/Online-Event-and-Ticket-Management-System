61b4586b2a86fcb2bd4086727639ab4e075d6613
Tran_Hoang_Vu <hoangvudx2004@gmail.com>
2025-08-20 10:28:46 +0700

    initial commit

---
+++ src/main/java/com/event/online_event_and_ticket_management_system/OnlineEventAndTicketManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.event.online_event_and_ticket_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEventAndTicketManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEventAndTicketManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/event/online_event_and_ticket_management_system/OnlineEventAndTicketManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.online_event_and_ticket_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEventAndTicketManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
